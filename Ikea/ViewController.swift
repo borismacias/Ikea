8ca37600e41f8af9acfb92fee5bff6c3064d3d75
Boris <bgonzalez@ing.uchile.cl>
2017-11-19 17:41:47 -0300

    :tada:

---
+++ Ikea/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Ikea
+//
+//  Created by Boris Alexis Gonzalez Macias on 11/19/17.
+//  Copyright © 2017 Boris Alexis Gonzalez Macias. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    let itemsArray = [ "cup", "vase", "boxing", "table" ]
+    @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var itemsCollectionView: UICollectionView!
+    let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.configuration.planeDetection = .horizontal
+        self.sceneView.session.run(configuration)
+        self.itemsCollectionView.dataSource = self
+        self.itemsCollectionView.delegate = self
+        self.registerGestureRecognizers()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func registerGestureRecognizers(){
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func tapped(){
+        print("Tapped the sceneview")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemsArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "item", for: indexPath) as! itemCell
+        cell.itemLabel.text = self.itemsArray[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.green
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.orange
+    }
+
+}
+
---
+++ Ikea/itemCell.swift
@@ -0,0 +1,14 @@
+//
+//  itemCell.swift
+//  Ikea
+//
+//  Created by Boris Alexis Gonzalez Macias on 11/19/17.
+//  Copyright © 2017 Boris Alexis Gonzalez Macias. All rights reserved.
+//
+
+import UIKit
+
+class itemCell: UICollectionViewCell {
+    
+    @IBOutlet weak var itemLabel: UILabel!
+}
